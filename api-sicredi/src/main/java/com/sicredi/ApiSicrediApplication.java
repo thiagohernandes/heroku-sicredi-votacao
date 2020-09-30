06e5bcaae2a6b2a47635d0915717ed3d872e588a
Thiago Hernandes <thernans@everis.com>
2020-09-30 16:36:29 -0300

    Criação do projeto

---
+++ api-sicredi/src/main/java/com/sicredi/ApiSicrediApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSicrediApplication.class, args);
+	}
+
+}
